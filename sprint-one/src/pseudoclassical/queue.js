fb031436bb76ac39e3a4349e78b9d5839817a368
Ben Blumenfeld <bmblumenfeld@gmail.com>
2018-01-17 22:35:48 -0800

    "passes all recs"

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.line = 0;
+  this.inLine = 0;
+  this.outLine = 0; 
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.inLine] = value;
+  this.line++;
+  this.inLine++;
+};
+
+Queue.prototype.dequeue = function () {
+  var nextUp = this.storage[this.outLine];
+  delete this.storage[this.outLine];
+  if (this.line !== 0) {
+    this.line--;  
+  }
+  this.outLine++;
+  return nextUp;
+};
+
+Queue.prototype.size = function () {
+  return this.line;
+};
 
