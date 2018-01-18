4465447cfae3424648021bb02150f35300d2ec05
Ben Blumenfeld <bmblumenfeld@gmail.com>
2018-01-17 22:52:38 -0800

    "all recs passes"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,32 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.line = 0;
+    this.inLine = 0;
+    this.outLine = 0; 
   }
 
+  enqueue (value) {
+    this.storage[this.inLine] = value;
+    this.line++;
+    this.inLine++;
+  };
+
+  dequeue () {
+    var nextUp = this.storage[this.outLine];
+    delete this.storage[this.outLine];
+    if (this.line !== 0) {
+      this.line--;  
+    }
+    this.outLine++;
+    return nextUp;
+   };
+
+  size () {
+    return this.line;
+  };
+
 }
+
+
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.stackCount = 0;
+  }
+
+  push (value) {
+    this.stackCount++;
+    this.storage[this.stackCount] = value;
+  }
+  
+  pop () {
+    var poppedVal = this.storage[this.stackCount + ''];
+    delete this.storage[this.stackCount];
+    if (this.stackCount !== 0) {
+      this.stackCount -= 1;
+    } 
+    return poppedVal;
+  }
+
+  size () {
+  return this.stackCount;	
   }
 
 }
+
+
+
+
