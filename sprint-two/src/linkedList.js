0a703c422ddc31c80eb9ac222675b49ee7f2c554
bmblumenfeld <bmblumenfeld@gmail.com>
2018-01-18 11:41:12 -0800

    "Issue with adding to tail"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,31 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var isFirst = true;
 
   list.addToTail = function(value) {
+    //initialize a objvar  - call node and pass val given
+    //objvar.next = list current tail
+    //list.tail = obj.var
+    var inputTail = Node(value);
+    inputTail.next = list.head;
+
+    list.tail = inputTail;
+
+    if (isFirst) {
+      list.head = inputTail;
+      isFirst = false; 
+    }
   };
 
   list.removeHead = function() {
+    var removedHead = list.head;
+    if (!removedHead.next) {
+      list.head = null;
+    } else {
+      list.head = removedHead.next;
+    } 
+    return removedHead.value;
   };
 
   list.contains = function(target) {
