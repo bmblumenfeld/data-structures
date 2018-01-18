c01dfaba427360e770c734f877e828e491b4502d
Ben Blumenfeld <bmblumenfeld@gmail.com>
2018-01-17 22:20:58 -0800

    "all recs pass"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.stackCount = 0;
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+  return someInstance;
 };
 
-var stackMethods = {};
+var storage = {}
 
+var stackMethods = {}
+
+stackMethods.push = function (value) {
+  this.stackCount++;
+  storage[this.stackCount] = value;
+}
+
+stackMethods.pop = function () {
+   var poppedVal = storage[this.stackCount + ''];
+    delete storage[this.stackCount];
+    if (this.stackCount !== 0) {
+      this.stackCount -= 1;
+    } 
+    return poppedVal;
+}
+
+stackMethods.size = function () {
+  return this.stackCount;	
+}
 
