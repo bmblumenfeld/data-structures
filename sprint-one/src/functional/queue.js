a5182019a23fb27d894cc4fe882e8e96eb82e199
Ben Blumenfeld <bmblumenfeld@gmail.com>
2018-01-17 17:56:48 -0800

    good through Functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,31 @@
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
+  // var order = Object.keys(storage).length;
+  var line = 0;
+  var inLine = 0;
+  var outLine = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[inLine] = value;
+    line++;
+    inLine++;
   };
 
   someInstance.dequeue = function() {
+    var nextUp = storage[outLine];
+    delete storage[outLine];
+    if (line ! == 0) {
+      line--;  
+    }
+    outLine++;
+    return nextUp;
   };
 
   someInstance.size = function() {
+    return line;
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,25 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var stackCount = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    stackCount++;
+    storage[stackCount] = value;
   };
 
   someInstance.pop = function() {
+    var poppedVal = storage[stackCount + ''];
+    delete storage.stackCount;
+    if (stackCount !== 0) {
+      stackCount -= 1;
+    } 
+    return poppedVal;
   };
 
   someInstance.size = function() {
+    return stackCount;  
   };
-
   return someInstance;
 };
