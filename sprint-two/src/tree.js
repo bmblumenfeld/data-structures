cb77b8ef57b43e240725ef8bacf357c875275a30
bmblumenfeld <bmblumenfeld@gmail.com>
2018-01-18 22:22:25 -0800

    Pass most tests for tree
    Only missing at this point how to
    assess contain for children within
    children.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,9 +14,36 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  var recurseChildren = function(node) {
+    return node.reduce(function (acc, element, index) {
+      if (element.value === target) {
+        acc = true;
+      }
+      if (element.children) {
+        recurseChildren(element.children);
+      } 
+      return acc;
+    }, false);
+    // if (!searThat) {
+    //   recurseChildren();
+    // }
+    
+  };
+
+  recurseChildren(this.children);
+  
+  // var inChildren = recurseChildren(this.children);
+  // while (this.children && !inChildren){
+    
+  // } 
+  
 };
 
 
