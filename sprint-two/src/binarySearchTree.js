ad6bc1f8eb7a39e680cb83842c5c604bf7e5f771
bmblumenfeld <bmblumenfeld@gmail.com>
2018-01-19 21:51:44 -0800

    Pass most tests for binarySearch
    Only missing test is depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,61 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left;
+  this.right;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode = new BinarySearchTree(value);
+  var compareNode = function(comparison) {
+    if (value > comparison.value) {
+      if (comparison.right) {
+        compareNode(comparison.right);
+      } else {
+        comparison.right = newNode;
+      }
+    } else {
+      if (comparison.left) {
+        compareNode(comparison.left);
+      } else {
+        comparison.left = newNode;
+      }
+    }
+  };
+  compareNode(this);
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+  var searchNode = function(comparison) {
+    if (value === comparison.value) {
+      return true;
+    } else if (value > comparison.value) {
+      if (comparison.right) {
+        return searchNode(comparison.right);
+      } else {
+        return false;
+      }
+    } else {
+      if (comparison.left) {
+        return searchNode(comparison.left);
+      } else {
+        return false;
+      }
+    }
+  };
+  return searchNode(this);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  var callOnNode = function(node) {
+    cb(this.value);
+    if (this.right) {
+      callOnNode(this.right);
+    } else if (this.left) {
+      callOnNode(this.left);
+    }    
+  };
+  callOnNode(this);
 };
 
 
