c91c5901aa8fd32553e27a9fc3f3e5c891f3803f
bmblumenfeld <bmblumenfeld@gmail.com>
2018-01-20 11:15:23 -0800

    Pass depthFirst test
    This (verbose) work passes the final test
    for binarySearchTree.  It finds the deepest
    node in the tree, stores the path to it, and
    then performs a callback on each item in the
    tree following that path, then the second
    deepest path, then the third, etc.  Plenty of
    refactoring and code cleanup is needed.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -47,18 +47,126 @@ BinarySearchTree.prototype.contains = function(value) {
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
-  var callOnNode = function(node) {
-    cb(this.value);
-    if (this.right) {
-      callOnNode(this.right);
-    } else if (this.left) {
-      callOnNode(this.left);
-    }    
+  var log = [];
+  var pathsStorage = {};
+  //var depthCt = 0;
+
+  var mapDepths = function(node, pathObj) {
+    if (node.right) {
+      var depthCt;      
+      if (!pathObj) {
+        depthCt = 0;
+      } else {
+        depthCt = pathObj.depth;
+      }
+      // var depthCt = pathObj[depthCt] || 0;
+      depthCt++;
+      var pathObj = pathObj || {};
+      pathObj.depth = depthCt;
+      //path.sequence = pathObj[sequence] ? pathObj[storage].push(node.value) : [node.value];
+      if (pathObj.hasOwnProperty('sequence')) {
+        pathObj['sequence'].push(node.value);
+      } else {
+        pathObj['sequence'] = [];
+        pathObj['sequence'].push(node.value);
+      }
+      //pathsStorage[node.value] = pathObj;
+
+      mapDepths(node.right, pathObj);
+      pathObj = null;
+    }
+    if (node.left) {
+      var depthCt;      
+      if (!pathObj) {
+        depthCt = 0;
+      } else {
+        depthCt = pathObj.depth;
+      }
+      // var depthCt = pathObj[depthCt] || 0;
+      depthCt++;
+      pathObj = pathObj || {};
+      pathObj.depth = depthCt;
+      //path.sequence = pathObj[sequence] ? pathObj[storage].push(node.value) : [node.value];
+      if (pathObj.hasOwnProperty('sequence')) {
+        pathObj['sequence'].push(node.value);
+      } else {
+        pathObj['sequence'] = [];
+        pathObj['sequence'].push(node.value);
+      }
+      //pathsStorage[node.value] = pathObj;
+
+      mapDepths(node.left, pathObj);
+      pathObj = null;
+    }
+
+    if (!node.left && !node.right) {
+      var depthCt;      
+      if (!pathObj) {
+        depthCt = 0;
+      } else {
+        depthCt = pathObj.depth;
+      }
+      // var depthCt = pathObj[depthCt] || 0;
+      depthCt++;
+      pathObj = pathObj || {};
+      pathObj.depth = depthCt;
+      //path.sequence = pathObj[sequence] ? pathObj[storage].push(node.value) : [node.value];
+      if (pathObj.hasOwnProperty('sequence')) {
+        pathObj['sequence'].push(node.value);
+      } else {
+        pathObj['sequence'] = [];
+        pathObj['sequence'].push(node.value);
+      }
+      pathsStorage[node.value] = pathObj;
+      depthCt = 0;
+      pathObj = null;
+    }
+
   };
-  callOnNode(this);
+  
+  mapDepths(this);
+
+  var outputPath = function() {
+    var maxDepth = 0;
+    var outputFirst;
+    var keyOfMax;
+    for (var key in pathsStorage) {
+      if (pathsStorage[key].depth > maxDepth) {
+        maxDepth = pathsStorage[key].depth;
+        outputFirst = pathsStorage[key].sequence;
+        keyOfMax = key;
+      }
+    }
+    outputFirst.forEach(function(item) {
+      log.push(item);
+    });
+    delete pathsStorage[keyOfMax];
+    // outputPath();
+    if (Object.keys(pathsStorage).length) {
+      outputPath(); 
+    }
+  };
+
+  outputPath();
+
+  var finalPath = _.uniq(log);
+  finalPath.forEach(function(element) {
+    cb(element);
+  });
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
+
+
+
